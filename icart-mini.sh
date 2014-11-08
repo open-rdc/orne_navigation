c33392b88e49aa0a7b81a857a3114eb2e8a9ab69
Daiki Maekawa <method_aspect_card@yahoo.co.jp>
2014-11-09 02:31:57 +0900

    Update icart-mini.sh

--- icart-mini.sh
+++ icart-mini.sh
@@ -4,7 +4,7 @@ do
 isAlive=`ps -ef | grep " ypspur-coordinator " | \
 grep -v grep | wc -l`
 if [ $isAlive = 1 ]; then
-: #Idling
+: #pass
 else
 echo "ypspur restart"
 ypspur-coordinator -p icart-mini.param -d /dev/sensors/icart-mini &
