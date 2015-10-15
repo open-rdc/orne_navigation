223aa6bb378ee9306ab7c24f86db7dca1d0b5d7c
humio <kappasetuhirobunbun@gmail.com>
2015-10-15 14:51:15 +0900

    add readFile

--- stoppoint_manager/src/stoppoint_sender.cpp
+++ stoppoint_manager/src/stoppoint_sender.cpp
@@ -1,7 +1,21 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 #include <geometry_msgs/PointStamped.h> 
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
+
+#include <yaml-cpp/yaml.h>
+
+#include <fstream>
+#include <string>
+
+#ifdef NEW_YAMLCPP
+template<typename T>
+void operator >> (const YAML::Node& node, T& i)
+{
+    i = node.as<T>();
+}
+#endif
 
 class StopPointSender{
   public:
@@ -17,6 +31,7 @@ class StopPointSender{
     geometry_msgs::PointStamped stop_point;
     bool done_sending_the_topic;
 
+    bool readFile(const std::string &filename);
     void sleep();
     void setStopArea(double xmin, double ymin, double xmax, double ymax);
     bool onStopArea();
@@ -35,12 +50,79 @@ StopPointSender::StopPointSender() :
   double max_update_rate;
   private_nh.param("max_update_rate", max_update_rate, 10.0);
   rate_ = ros::Rate(max_update_rate);
+  std::string filename = "";
+  private_nh.param("filename", filename, filename);
+  if(filename != ""){
+    ROS_INFO_STREAM("Read waypoints data from " << filename);
+    readFile(filename);
+  }
 
   ros::NodeHandle nh;
-  stop_point_pub_ = nh.advertise<geometry_msgs::PointStamped>("/stoppoint", 1);
+  stop_point_pub_ = nh.advertise<geometry_msgs::PointStamped>("/stop_point", 1);
 
-  setStopPoint(-0.856632, 5.10851, -2.68494e-10);
-  setStopArea(-1.51892, 8.49779, -0.51892, 9.49779);
+  // setStopPoint(-0.856632, 5.10851, -2.68494e-10);
+  // setStopArea(-1.51892, 8.49779, -0.51892, 9.49779);
+}
+
+bool StopPointSender::readFile(const std::string &filename)
+{
+  try{
+    std::ifstream ifs(filename.c_str(), std::ifstream::in);
+    if(ifs.good() == false){
+        return false;
+    }
+
+    YAML::Node node;
+    
+    #ifdef NEW_YAMLCPP
+        node = YAML::Load(ifs);
+    #else
+        YAML::Parser parser(ifs);
+        parser.GetNextDocument(node);
+    #endif
+
+    #ifdef NEW_YAMLCPP
+        const YAML::Node &sa_node_tmp = node["stop_area"];
+        const YAML::Node *sa_node = sa_node_tmp ? &sa_node_tmp : NULL;
+    #else
+        const YAML::Node *sa_node = node.FindValue("stop_area");
+    #endif
+
+    if(sa_node != NULL){
+      double xmin, ymin, xmax, ymax;
+
+      (*sa_node)["area"]["xmin"] >> xmin;
+      (*sa_node)["area"]["ymin"] >> ymin;
+      (*sa_node)["area"]["xmax"] >> xmax;
+      (*sa_node)["area"]["ymax"] >> ymax; 
+
+      setStopArea(xmin, ymin, xmax, ymax);
+    }else{
+        return false;
+    }
+
+    const YAML::Node &sp_node_tmp = node["stop_point"];
+    const YAML::Node *sp_node = sp_node_tmp ? &sp_node_tmp : NULL;
+
+    if(sp_node != NULL){
+        double x, y, z;
+        (*sp_node)["point"]["x"] >> x;
+        (*sp_node)["point"]["y"] >> y;
+        (*sp_node)["point"]["z"] >> z;
+
+        setStopPoint(x, y, z);
+    }else{
+        return false;
+    }
+
+  }catch(YAML::ParserException &e){
+      return false;
+
+  }catch(YAML::RepresentationException &e){
+      return false;
+  }
+
+  return true;
 }
 
 void StopPointSender::run(){
